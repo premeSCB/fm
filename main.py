ef057575e402e3bda7a61ec88d5131dd1279839a
premeSCB <t_Napat.Warasittichai@scb.co.th>
2025-06-26 09:56:39 +0700

    Update main.py
    blocked by firewall, cannot download data from yfinance

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import yfinance as yf
+from scipy.stats import norm
+
+ticker_symbol = 'AAPL'
+data = yf.download(ticker_symbol)
